efc70857ffaddd00563fd5aecf025e1b228cb037
mn12you <76604100+mn12you@users.noreply.github.com>
2022-12-29 14:17:49 +0800

    Create extrrct.py

---
+++ extrrct.py
@@ -0,0 +1,13 @@
+import pywt
+
+from biosppy import ecg, tools
+import numpy as np
+import pandas as pd
+import scipy
+
+class extract():
+    def M(self):
+        pass 
+class E(extract):
+    def M(self):
+        pass
